89f47c857b2928f7160ee587e30bfae517f5b02d
Dilmi Rathnayake <dilmithashmilarathnayake@gmail.com>
2026-01-14 21:29:56 +0530

    Add files via upload

---
+++ pages/news/index.js
@@ -0,0 +1,22 @@
+//our-domain.com/news
+
+import Link from "next/link";
+import React, { Fragment } from "react";
+
+function NewsPage(){
+    return(
+    <Fragment>
+    <h1>The News Page</h1>
+    <ul>
+        <li>
+           <Link href="/news/nextjs-is-a-great-framework"> 
+                NextJS is a great framework
+           </Link>
+            </li>
+        <li>Something else</li>
+    </ul>
+    </Fragment>
+    );
+}
+
+export default NewsPage;
